3bd384e417268d17ae365c8d16f512613305a143
cha1ra <s11397as@cha1ra.com>
2018-10-16 15:19:11 +0900

    関数を変数に代入もできるよ

--- js/script.js
+++ js/script.js
@@ -3,8 +3,20 @@ let currentYear = 2018;
 
 console.log(tokyoOlympic - currentYear);
 
-$('.hero-image__text').html('<h1>ハリルホジッチ<h1>');
-$('#about').fadeOut(3000).fadeIn(3000);
+
 
 console.log("test");
 
+const doFunc = function(){
+    $('.hero-image__text').html('<h1>ハリルホジッチ<h1>');
+    $('#about').fadeOut(3000).fadeIn(3000);
+}
+
+$("#btn").on('click', doFunc);
+
+/*
+$("#btn").on('click', function(){
+    $('.hero-image__text').html('<h1>ハリルホジッチ<h1>');
+    $('#about').fadeOut(3000).fadeIn(3000);
+});
+*/
