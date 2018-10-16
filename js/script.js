e4772e5518d70dab8dbd24fd027e073e9b11b208
cha1ra <s11397as@cha1ra.com>
2018-10-16 16:44:45 +0900

    menubar scroll functuin with jQuery

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ let currentYear = 2018;
 console.log(tokyoOlympic - currentYear);
 console.log("test");
 
-
+/*
 const pageScroll = function(){
     const aboutFromTop = $("#about").offset().top;
     console.log("about!!");
@@ -12,4 +12,17 @@ const pageScroll = function(){
     console.log(aboutFromTop);
 };
 
+
 $('#aboutlink').on('click', pageScroll);
+*/
+
+const pageScroll = function(){
+    const href = $(this).attr('href');
+    console.log(href);
+    const aboutFromTop = $(href).offset().top;
+    $('html, body').animate({scrollTop:aboutFromTop},1000);
+    console.log(aboutFromTop);
+};
+
+
+$('.nav-item > a').on('click', pageScroll);
