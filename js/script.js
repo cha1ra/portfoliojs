79d3dc14024a27b7b52a96ddd6a79cdcb2808b95
cha1ra <s11397as@cha1ra.com>
2018-10-16 16:11:24 +0900

    scroll menu

--- js/script.js
+++ js/script.js
@@ -2,31 +2,14 @@ const tokyoOlympic = 2020;
 let currentYear = 2018;
 
 console.log(tokyoOlympic - currentYear);
-
-
-
 console.log("test");
 
-const doFunc = function(){
-    const time = new Date();
 
-    //getMonth(), getDate(), getDay()
-    const year = time.getFullYear();
+const pageScroll = function(){
+    const aboutFromTop = $("#about").offset().top;
+    console.log("about!!");
+    $('html, body').animate({scrollTop:aboutFromTop},1000);
+    console.log(aboutFromTop);
+};
 
-    console.log(year);
-
-    const link = '<a href = "https://www.cha1ra.com">ジーズアカデミー </a>';
-    $('.hero-image__text').html(link);
-
-    const animationTime = 2000;
-    $('#message').fadeOut(animationTime).fadeIn(animationTime);
-}
-
-$("#btn").on('click', doFunc);
-
-/*
-$("#btn").on('click', function(){
-    $('.hero-image__text').html('<h1>ハリルホジッチ<h1>');
-    $('#about').fadeOut(3000).fadeIn(3000);
-});
-*/
+$('#aboutlink').on('click', pageScroll);
