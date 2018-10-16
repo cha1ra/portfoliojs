d287b7a854e6cfe300abd23c19c9782c8253eb6e
cha1ra <s11397as@cha1ra.com>
2018-10-16 15:25:18 +0900

    Add Date class

--- js/script.js
+++ js/script.js
@@ -8,8 +8,18 @@ console.log(tokyoOlympic - currentYear);
 console.log("test");
 
 const doFunc = function(){
-    $('.hero-image__text').html('<h1>ハリルホジッチ<h1>');
-    $('#about').fadeOut(3000).fadeIn(3000);
+    const time = new Date();
+
+    //getMonth(), getDate(), getDay()
+    const year = time.getFullYear();
+
+    console.log(year);
+
+    const link = '<a href = "https://www.cha1ra.com">ジーズアカデミー </a>';
+    $('.hero-image__text').html(link);
+
+    const animationTime = 2000;
+    $('#message').fadeOut(animationTime).fadeIn(animationTime);
 }
 
 $("#btn").on('click', doFunc);
